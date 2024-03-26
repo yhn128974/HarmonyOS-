aee25f7fca9315635e004799c3930f7de4e81d80
yhn128974 <yuethanhaonan@outlook.com>
2024-03-26 20:18:37 +0800

    harmonyOs day8 exercise

--- entry/src/main/ets/myabilityStage/MyAbilityStage.ts
+++ entry/src/main/ets/myabilityStage/MyAbilityStage.ts
@@ -1,13 +1,11 @@
 import { AbilityStage, Want } from '@kit.AbilityKit';
 
 
-export  default class MyAbilityStage extends AbilityStage{
+export default class MyAbilityStage extends AbilityStage {
   onAcceptWant(want: Want): string {
-    if (want.abilityName==='DocumentAbility'){
+    if (want.abilityName === 'DocumentAbility') {
       return `DocAbilityInstance_${want.parameters.instanceKey}`
     }
-
     return ''
-
   }
 }
